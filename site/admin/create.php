fb8f19f171490f7319a162c7be57cd38f3523255
Abdessamad ELMAZGHY <57547357+aelmazghy@users.noreply.github.com>
2020-06-09 15:29:01 +0200

    Create new - WIP

--- site/admin/create.php
+++ site/admin/create.php
@@ -13,9 +13,10 @@ $news->setFetchMode(PDO::FETCH_ASSOC);
         </ul>
     </div>
     <div id="demonews">
-    <div class="col-xs-12 col-lg-8 add-news float-left">
-        <div class="card-box">
+    <div class="col-xs-12 col-lg-6 add-news float-left">
+
             <form>
+                <div class="card-box">
                 <div class="form-group">
                     <label for="new-Titre">Titre</label>
                     <input type="text" class="form-control" id="new-Titre" placeholder="Titre new" v-model="title">
@@ -27,44 +28,59 @@ $news->setFetchMode(PDO::FETCH_ASSOC);
                 <div class="form-group">
                     <label for="new-Image">Image</label>
                     <div class="custom-file">
-                        <input type="file" id="imageLoader" @change="updateCanvasImage"/>
+                        <input type="file" class="form-control" id="imageLoader" @change="updateCanvasImage"/>
                         <label class="custom-file-label" for="imageLoader">Ajouter une image</label>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="new-texteAlternatif">Texte Alternatif</label>
+                    <label for="new-texteAlternatif">Texte Alternatif Image</label>
                     <input type="text" class="form-control" id="new-texteAlternatif" placeholder="Texte Alternatif">
                 </div>
+                </div><!-- /.card-box -->
+                <div class="card-box">
                 <div class="form-group">
                     <label for="new-typeLink">Type du lien</label>
-                    <select class="form-control" id="new-typeLink">
-                        <option selected="selected">Aucun</option>
-                        <option>Interne</option>
-                        <option>Externe</option>
-                        <option>PDF</option>
+                    <select class="form-control" id="new-typeLink" v-model="typeLinkSelected">
+                        <option
+                                v-for="typelink in typeLinks"
+                                :value="typelink"
+                                :key="typelink.id">
+                                {{ typelink.name }}
+                        </option>
                     </select>
                 </div>
+                <div class="hasLink" v-if="typeLinkSelected.name != 'Aucun'">
                 <div class="form-group">
                     <label for="new-textLink">Texte du lien</label>
                     <input type="text" class="form-control" id="new-textLink" placeholder="Ex: Découvrir" v-model="urltext">
                 </div>
                 <div class="form-group">
                     <label for="new-urlLink">URL</label>
-                    <input type="text" class="form-control" id="new-urlLink" placeholder="http://croustillance.com/decouvrir">
+                    <input v-if="typeLinkSelected.name === 'Externe' || typeLinkSelected.name === 'PDF'" type="text" class="form-control" id="new-urlLink" placeholder="http://croustillance.com/decouvrir">
+
+                    <select v-if="typeLinkSelected.name === 'Interne'" class="form-control" id="new-urlLink">
+                        <option
+                        v-for="internalLink in internalLinks"
+                        :value="internalLink">
+                            {{ internalLink.path }}
+                        </option>
+                    </select>
+
+
+                </div>
                 </div>
 
-                <div class="text-right">
+                </div><!-- /.card-box -->
+
+                <div class="text-center">
                     <button type="button" class="btn btn-primary">Ajouter</button>
                 </div>
             </form>
-        </div><!-- /.card-box -->
+
     </div>
-<div class="col-xs-12 col-lg-4 side-apercu float-right">
+<div class="col-xs-12 col-lg-6 side-apercu float-right">
     <div class="card-box">
-
-
-
-            <div class="section fp-auto-height fp-section active section-anim fp-completely" data-section="projects-grid" data-anchor="projects-grid" style="height: 627px;">
+            <div class="section fp-auto-height fp-section active section-anim fp-completely" data-section="projects-grid" data-anchor="projects-grid">
                 <!-- Begin of section wrapper -->
                 <div class="section-wrapper fullwidth with-margin">
                     <!-- content -->
@@ -78,12 +94,11 @@ $news->setFetchMode(PDO::FETCH_ASSOC);
                                     <div class="mask"></div>
                                     <img src="" alt="" class="img-block" style="display: none;">
                                     <canvas id="imageCanvas" ref="imageCanvas"></canvas>
-                                    <div class="img" alt="news.imgAlt"></div>
                                 </div>
                                 <div class="media-body">
                                     <a href="#">
-                                        <h2 class="sr-up-td1">33/33/3333</h2>
-                                        <h2 class="sr-up-td2 display-6 mt-5 mb-5">{{ title }}</h2>
+                                        <h2 class="sr-up-td1">{{ formatDate() }}</h2>
+                                        <h2 class="sr-up-td2 display-6 mt-3 mb-3">{{ title }}</h2>
                                     </a>
                                     <div class="desc">
                                         <p class="pSpacer" style="background-color:rgba(218, 218, 218, 0.72);color: #fff;border-radius: 5px;">
@@ -92,14 +107,12 @@ $news->setFetchMode(PDO::FETCH_ASSOC);
                                     </div>
                                 </div>
 
-                                <div class="media-footer sr-up-td4">
+                                <div class="media-footer sr-up-td4" v-if="typeLinkSelected.name != 'Aucun'">
                                     <div class="btns-action sr-up-td3 text-primary">
                                         <a class="btn btn-normal btn-white spaceTop">
-                                            <span class="icon"><img src="img/pdf-icone.svg" width="35px"></span>
+                                            <span class="icon" v-if="typeLinkSelected.name == 'PDF'"><img src="img/pdf-icone.svg" width="35px"></span>
                                             <span class="text">{{urltext}}</span>
-                                            <span class="icon">
-                                            <span class="arrow-right"></span>
-                                        </span>
+                                            <span class="icon" v-if="typeLinkSelected.name === 'Interne' || typeLinkSelected.name === 'Externe'"><span class="arrow-right"></span></span>
                                         </a>
                                     </div>
                                 </div>
@@ -119,15 +132,50 @@ $news->setFetchMode(PDO::FETCH_ASSOC);
     <script src="https://unpkg.com/vue-upload-multiple-image@1.0.2/dist/vue-upload-multiple-image.js"></script>
     <!--end sidebar div-->
     <script>
+
+
+
         new Vue({
             el: '#demonews',
             data: {
                 title: '',
                 description: '',
                 urltext: '',
-                imag: ''
+                imag: '',
 
+                typeLinks: [
+                    {name: 'Aucun', id: 1},
+                    {name: 'Interne', id: 2},
+                    {name: 'Externe', id: 3},
+                    {name: 'PDF', id: 4}
+                ],
+                typeLinkSelected: 1,
 
+                internalLinks: [
+                    { path: '/' },
+                    { path: '/buffet-traiteur' },
+                    { path: '/buffet-entreprise' },
+                    { path: '/cocktail-professionnels' },
+                    { path: '/cocktail-particuliers' },
+                    { path: '/contact' },
+                    { path: '/equipe' },
+                    { path: '/esprit' },
+                    { path: '/location-materiel-traiteur' },
+                    { path: '/evenements-entreprises' },
+                    { path: '/traiteur-mariage' },
+                    { path: '/pause-dejeuner' },
+                    { path: '/portage-repas' },
+                    { path: '/repas-particuliers' },
+                    { path: '/repas-professionnels' },
+                    { path: '/label-vosges-terroir' },
+                    { path: '/actualites' },
+                    { path: '/plateaux-repas-entreprise' }
+
+                ],
+
+            },
+            created: function() {
+                this.typeLinkSelected = this.typeLinks.find(i => i.id === this.typeLinkSelected);
             },
             methods: {
             updateCanvasImage(e) {
@@ -157,8 +205,12 @@ $news->setFetchMode(PDO::FETCH_ASSOC);
 
                 var ctx = canvas.getContext('2d');
                 ctx.drawImage(img,0,0);
-            }
-        }
+            },
+                formatDate(dateEvent){
+                    moment.locale('fr');         // fr
+                    return moment(dateEvent).format('LL');  // Jun 2, 2020 11:44 AM
+                }
+        },
         })
     </script>
 <?php include "footer.php";?>
--- site/admin/header.php
+++ site/admin/header.php
@@ -10,7 +10,7 @@ include "connectdb.php"
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="public/css/style.css"/>
+    <link rel="stylesheet" href="css/demonew.css"/>
     <link rel="stylesheet" href="css/style.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css"/>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -18,6 +18,7 @@ include "connectdb.php"
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
     <script type="text/javascript" src="js/table.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment-with-locales.min.js" crossorigin="anonymous"></script>
 
     <title>Croustillance - Liste actualités</title>
 </head>
